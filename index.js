5143f682e9073dc81523aae4ad2223f3875124cf
Diamond Toliver <diamondtoliver@Diamonds-MacBook-Pro.local>
2021-12-16 16:07:12 -0500

    added rollbar

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '39d7dc3d5f19463c82545adb848ba74a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file serverd successfully.')
+})
+
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
